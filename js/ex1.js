af4cdfcd476eb18187deb1b5c47bf1e86dacfbd7
Brentaldam <brent.marriott@student.csulb.edu>
2022-09-15 19:33:31 -0700

    "msg"

--- js/ex1.js
+++
@@ -1,9 +0,0 @@
-
-console.log("Marriott's Output from Example 1");
-const number = Number(prompt("Enter a number 1-100:"));
-if ((number >= 0) && (number <= 100)) {
-    console.log(`Thank you! You entered ${number}, a valid number.`);
-  }
-else{
-    console.log(`Sorry, ${number} is not a valid entry.`);
-}
--- js/ex6.js
+++
@@ -1,6 +0,0 @@
-console.log("Marriott's Output from Example 6");
-const number = Number(prompt("Enter a number:"));
-for (let i = 0; i < 11; i++) {
-    ans = number * i;
-console.log(`${number} x ${i} = ${ans}`)
-}
